9f6a86c73c00860b6bd30b3ffbb9034f4e16ffcf
Jaroslav Bonco <xbonco1@nymfe77.fi.muni.cz>
2017-10-26 15:23:50 +0200

    Fixed some typos

--- carparkmanager-persistence/src/test/java/cz/muni/fi/pa165/carparkmanager/persistence/CarDaoTest.java
+++ carparkmanager-persistence/src/test/java/cz/muni/fi/pa165/carparkmanager/persistence/CarDaoTest.java
@@ -15,8 +15,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 
 /**
