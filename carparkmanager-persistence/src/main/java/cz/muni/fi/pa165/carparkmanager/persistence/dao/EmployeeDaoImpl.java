a11708051b55aa7c57979143f6d82a11e6830426
Jaroslav Bonco <xbonco1@nymfe77.fi.muni.cz>
2017-10-26 15:21:04 +0200

    Implementation of my part - Employee entity and CarDao tests

---
+++ carparkmanager-persistence/src/main/java/cz/muni/fi/pa165/carparkmanager/persistence/dao/EmployeeDaoImpl.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.carparkmanager.persistence.dao;
+
+import cz.muni.fi.pa165.carparkmanager.persistence.entity.Employee;
+import enums.ClassificationOfEmployee;
+import java.util.Calendar;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author xbonco1
+ */
+@Repository
+public class EmployeeDaoImpl implements EmployeeDao {
+
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Override
+    public Employee create(Employee employee) throws IllegalArgumentException {
+        if(parametersAreOk(employee))
+        {
+            em.persist(employee);
+            em.flush();
+            return employee;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Employee cannot have null parameter");
+        }
+    }
+
+    @Override
+    public Employee update(Employee employee) throws IllegalArgumentException {
+        if (parametersAreOk(employee)){
+            em.merge(employee);
+            return employee;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Employee cannot have null parameter");
+        }
+    }
+
+    @Override
+    public void delete(Employee employee) {
+        if (parametersAreOk(employee)){
+            em.remove(em.contains(employee) ? employee : em.merge(employee));
+        }
+        em.remove(em.contains(employee) ? employee : em.merge(employee));
+    }
+
+    @Override
+    public Employee findById(Long id) {
+        return em.find(Employee.class, id);
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        return em.createQuery("select person from Person person").getResultList();
+    }
+
+    @Override
+    public List<Employee> findByFirstname(String firstname) {
+        if (firstname == null) {
+            throw new IllegalArgumentException("Cannot search for null firstname");
+        }
+        
+        return em.createQuery("SELECT employee FROM Employee employee WHERE employee.firstname = :firstname")
+                .setParameter("firstname", firstname)
+                .getResultList();
+    }
+
+    @Override
+    public List<Employee> findBySurname(String surname) {
+        if (surname == null) {
+            throw new IllegalArgumentException("Cannot search for null surname");
+        }
+        
+        return em.createQuery("SELECT employee FROM Employee employee WHERE employee.surname = :surname")
+                .setParameter("surname", surname)
+                .getResultList();
+    }
+
+    @Override
+    public List<Employee> findByClassification(ClassificationOfEmployee classification) {
+        if (classification == null) {
+            throw new IllegalArgumentException("Cannot search for classification login");
+        }
+        
+        return em.createQuery("SELECT employee FROM Employee employee WHERE employee.classification = :classification")
+                .setParameter("classification", classification).getResultList();
+    }
+    
+    private boolean parametersAreOk(Employee employee)
+    {
+        if(employee == null || employee.getFirstname() == null || employee.getSurname() == null || employee.getBirthDate() == null || employee.getClassification() == null){
+            return false;
+        }
+        
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(employee.getBirthDate());
+        
+        int yearOfEmployeeBirth = cal1.get(Calendar.YEAR);
+        int currentYear = cal2.get(Calendar.YEAR);
+        
+        if(currentYear - yearOfEmployeeBirth < 18)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+}
---
+++ carparkmanager-persistence/src/main/java/cz/muni/fi/pa165/carparkmanager/persistence/entity/Employee.java
@@ -0,0 +1,139 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.carparkmanager.persistence.entity;
+
+import enums.ClassificationOfEmployee;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author xbonco1
+ */
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name="EMPLOYEE_ID")
+    private Long id;
+    
+    @NotNull
+    @Column(nullable = false)
+    private String firstname;
+
+    @NotNull
+    @Column(nullable = false)
+    private String surname; 
+    
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @NotNull
+    @Enumerated
+    private ClassificationOfEmployee classification;    
+
+    public Employee() {
+        
+    }
+    
+    public Employee(Long id, String firstname, String surname, Date birthDate, ClassificationOfEmployee classification) {
+        this.id = id;
+        this.firstname = firstname;
+        this.surname = surname;
+        this.birthDate = birthDate;
+        this.classification = classification;         
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getFirstname() {
+        return firstname;
+    }
+    
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date dateOfBirth) {
+        this.birthDate = dateOfBirth;
+    }
+
+    public ClassificationOfEmployee getClassification() {
+        return classification;
+    }
+
+    public void setClassification(ClassificationOfEmployee classification) {
+        this.classification = classification;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 73 * hash + Objects.hashCode(this.id);
+        hash = 73 * hash + Objects.hashCode(this.birthDate);
+        hash = 73 * hash + Objects.hashCode(this.firstname);
+        hash = 73 * hash + Objects.hashCode(this.surname);
+        hash = 73 * hash + Objects.hashCode(this.classification);
+
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (!(obj instanceof Employee)) {
+            return false;
+        }
+        
+        final Employee other = (Employee) obj;
+        if (!Objects.equals(this.id, other.getId())) {
+            return false;
+        }
+        if (!Objects.equals(this.firstname, other.getFirstname())) {
+            return false;
+        }
+        if (!Objects.equals(this.surname, other.getSurname())) {
+            return false;
+        }
+        if (!Objects.equals(this.birthDate, other.getBirthDate())) {
+            return false;
+        }
+        if (this.classification != other.getClassification()) {
+            return false;
+        }
+        
+        return true;
+    }
+}
