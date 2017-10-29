a52b8ada260ce0a9f21239e583d3ad94fb91df0b
josef <josef@DESKTOP-EN9J8L9>
2017-10-29 16:44:19 +0100

    Add Test

---
+++ carparkmanager-persistence/src/test/java/cz/muni/fi/pa165/carparkmanager/persistence/DriveDaoTest.java
@@ -0,0 +1,79 @@
+package cz.muni.fi.pa165.carparkmanager.persistence;
+
+import cz.muni.fi.pa165.carparkmanager.persistence.dao.DriveDao;
+import cz.muni.fi.pa165.carparkmanager.persistence.entity.Drive;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Josef Marek
+ */
+@ContextConfiguration(classes=ConfigurationPersistence.class) // TODO
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class DriveDaoTest extends AbstractTestNGSpringContextTests {
+    @Autowired
+    private DriveDao driveDao;
+    public static final int DRIVE_COUNT = 1;
+    private Drive drive1;
+    
+    SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+
+    @BeforeMethod
+    public void init() throws ParseException {
+        drive1= new Drive ();
+        drive1. setTimeFrom (sdf.parse("25.10.2017"));
+        drive1. setTimeTo (sdf.parse("28.10.2017"));
+        drive1. setKm (1038);        
+driveDao.create(drive1);
+    }
+
+    @Test
+    public void testFindDriveById() {
+        Assert.assertNotNull(driveDao.findById(drive1.getId()));
+    }
+
+    @Test
+    public void testFindDriveByKm () {
+        Assert.assertNotNull(driveDao.findByKm(1038));
+    }
+
+
+    
+    @Test
+    public void testFindAllDrive() {
+        Assert.assertEquals(DRIVE_COUNT, driveDao.findAll().size());
+    }
+
+    @Test
+    public void testDeleteDrive() {
+        driveDao.delete(drive1);
+        Assert.assertNull(driveDao.findById(drive1.getId()));
+    }
+
+    @Test
+    public void testUpdateDrive() {
+        drive1.setKm(1138);
+        driveDao.update(drive1);
+        Assert.assertEquals(drive1.getKm(), 1138);
+    }
+   
+    @Test
+    public void findAllDriveTest(){
+        driveDao.create(drive1);
+        List<Drive> cars = driveDao.findAll();
+        Assert.assertEquals(cars.size(), 1);
+        assertDeepEquals(drive.get(0), drive);
+      }
+}
