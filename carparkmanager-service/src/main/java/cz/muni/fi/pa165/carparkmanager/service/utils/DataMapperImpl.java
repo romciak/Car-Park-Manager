718689fa425b670a01bbed5da84d2641ecd23021
romciak <roman.nedelka@gmail.com>
2017-11-26 18:48:51 +0100

    Created mapping class, added dependecy, CarServiceImpl done.

---
+++ carparkmanager-service/src/main/java/cz/muni/fi/pa165/carparkmanager/service/utils/DataMapperImpl.java
@@ -0,0 +1,39 @@
+package cz.muni.fi.pa165.carparkmanager.service.utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.dozer.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Roman Nedelka
+ */
+@Service
+public class DataMapperImpl implements DataMapper {
+
+    @Autowired
+    private Mapper dozer;
+
+    @Override
+    public <T> List<T> mapTo(Collection<?> from, Class<T> to) {
+        List<T> mappedCollection = new ArrayList<>();
+        for (Object object : from) {
+            mappedCollection.add(dozer.map(object, to));
+        }
+        return mappedCollection;
+    }
+
+    @Override
+    public <T> T mapTo(Object from, Class<T> to) {
+        return dozer.map(from, to);
+    }
+
+    @Override
+    public Mapper getMapper() {
+        return dozer;
+    }
+
+}
