449e9e49e7c046fb7301b5a51b497208cff54621
romciak <roman.nedelka@gmail.com>
2017-11-26 19:13:20 +0100

    ServiceConfiguration class done.

--- carparkmanager-service/src/main/java/cz/muni/fi/pa165/carparkmanager/service/config/ServiceConfiguration.java
+++ carparkmanager-service/src/main/java/cz/muni/fi/pa165/carparkmanager/service/config/ServiceConfiguration.java
@@ -1,9 +1,26 @@
 package cz.muni.fi.pa165.carparkmanager.service.config;
 
+import cz.muni.fi.pa165.carparkmanager.persistence.conf.PersistenceApplicationContext;
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
 /**
  *
  * @author Roman Nedelka
  */
+@Configuration
+@Import(PersistenceApplicationContext.class)
+@ComponentScan(value = "cz.muni.fi.pa165.carparkmanager.service")
 public class ServiceConfiguration {
-    
+
+    @Bean
+    public Mapper dozer() {
+        DozerBeanMapper dozer = new DozerBeanMapper();
+        return dozer;
+    }
+
 }
