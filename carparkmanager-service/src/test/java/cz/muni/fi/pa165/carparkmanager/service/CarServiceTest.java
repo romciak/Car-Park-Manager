47c42e227542950bf20d7d42cfe9d53f3c3f1f16
romciak <roman.nedelka@gmail.com>
2017-11-30 19:01:52 +0100

    Non-trivial method tested, milestone done.

--- carparkmanager-service/src/test/java/cz/muni/fi/pa165/carparkmanager/service/CarServiceTest.java
+++ carparkmanager-service/src/test/java/cz/muni/fi/pa165/carparkmanager/service/CarServiceTest.java
@@ -6,11 +6,17 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import cz.muni.fi.pa165.carparkmanager.persistence.dao.CarDao;
 import cz.muni.fi.pa165.carparkmanager.persistence.dao.DriveDao;
+import cz.muni.fi.pa165.carparkmanager.persistence.dao.EmployeeDao;
 import cz.muni.fi.pa165.carparkmanager.persistence.dao.ServiceCheckDao;
 import cz.muni.fi.pa165.carparkmanager.persistence.entity.Car;
+import cz.muni.fi.pa165.carparkmanager.persistence.entity.Employee;
 import cz.muni.fi.pa165.carparkmanager.persistence.entity.ServiceCheck;
+import cz.muni.fi.pa165.carparkmanager.persistence.enums.ClassificationOfEmployeesEnum;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import org.apache.commons.lang3.time.DateUtils;
 import org.hibernate.service.spi.ServiceException;
 import org.mockito.InjectMocks;
 import static org.mockito.Matchers.any;
@@ -29,21 +35,29 @@ import static org.mockito.Mockito.when;
 @ContextConfiguration(classes = ServiceConfiguration.class)
 public class CarServiceTest extends AbstractTestNGSpringContextTests {
 
+    private static final long ONE_MINUTE_IN_MILIS = 600000;
+
     @Mock
     private CarDao carDao;
-    
+
     @Mock
     private ServiceCheckDao serviceCheckDao;
-    
+
     @Mock
     private DriveDao driveDao;
 
+    @Mock
+    private EmployeeDao employeeDao;
+
     @InjectMocks
     private final CarService carService = new CarServiceImpl();
-    
+
+    @InjectMocks
+    private final EmployeeService employeeService = new EmployeeServiceImpl();
+
     @InjectMocks
     private final ServiceCheckService serviceCheckService = new ServiceCheckServiceImpl();
-    
+
     @InjectMocks
     private final DriveService driveService = new DriveServiceImpl();
 
@@ -54,9 +68,21 @@ public class CarServiceTest extends AbstractTestNGSpringContextTests {
 
     private Car car1;
     private Car car2;
+    private ServiceCheck serviceCheck1;
+    private Employee emp;
 
     @BeforeMethod
     public void prepare() {
+
+        Date date = new Date();
+        Date datePlusSixMonths = DateUtils.addMonths(date, 6);
+
+        serviceCheck1 = new ServiceCheck();
+        serviceCheck1.setId(10L);
+        serviceCheck1.setDone(true);
+        serviceCheck1.setDoneWhen(date);
+        serviceCheck1.setIntervalFrom(date);
+        serviceCheck1.setIntervalTo(datePlusSixMonths);
 
         car1 = new Car();
         car2 = new Car();
@@ -75,6 +101,18 @@ public class CarServiceTest extends AbstractTestNGSpringContextTests {
         car2.setEngineType("Perfection");
         car2.setKmCount(30000);
         car2.setProductionYear("2016");
+
+        List serviceChecks = new ArrayList();
+        serviceChecks.add(serviceCheck1);
+        car2.setServiceCheckList(serviceChecks);
+
+        emp = new Employee();
+        emp.setBirthDate(new Date());
+        emp.setClassification(ClassificationOfEmployeesEnum.MANAGER);
+        emp.setFirstname("Roman");
+        emp.setSurname("Nedelka");
+        emp.setId(1L);
+        emp.setDriveList(null);
     }
 
     @Test
@@ -110,10 +148,21 @@ public class CarServiceTest extends AbstractTestNGSpringContextTests {
         final List<Car> cars = carService.findAll();
         Assert.assertEquals(2, cars.size());
     }
-    
-    @Test // TODO
+
+    @Test
     public void carWithoutPreviousServiceCheckTest() {
         ServiceCheck sc = carService.checkServiceInterval(car1);
         Assert.assertNotNull(sc);
+    }
+
+    @Test
+    public void carWithPreviousServiceCheckTest() {
+        carDao.create(car2);
+        ServiceCheck sc = carService.checkServiceInterval(car2);
+
+        Date date = new Date();
+        Date datePlusSixMonths = DateUtils.addMonths(date, 6);
+
+        Assert.assertTrue((datePlusSixMonths.getTime() - sc.getIntervalFrom().getTime()) < ONE_MINUTE_IN_MILIS);
     }
 }
