19793e08e10ccd07eafdcac512c27d2a78cbafce
Jakub Juřena <jakub.jurena@gmail.com>
2017-12-18 18:14:34 +0100

    dependencies fixed

--- carparkmanager-angular/src/main/resources/static/js/app.js
+++ carparkmanager-angular/src/main/resources/static/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('app', ['ngRoute', 'ngResource', 'ui.bootstrap']);
+var app = angular.module('app', ['ngRoute', 'ngResource']);
 
 app.config(function($routeProvider, $locationProvider){
     $locationProvider.html5Mode(true);
